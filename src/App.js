e280f8139cd3ec8bee19459f104216cb1c31e55a
Maikel <maikel863@outlook.com>
2021-10-14 04:45:19 -0400

    created first component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Header from './components/Header';
+import NavBar from './components/NavBar';
+
 import './App.css';
 
 function App() {
   return (
+    <Router>
     <div className="App">
+      <div className="navbar">
+        <NavBar />
+        </div>
+
+    <Switch>
+      <Route exact path="/">
+        <Header />
+      </Route>
+    </Switch>
 
     </div>
+    </Router>
   );
 }
 
---
+++ src/components/TacoCard.js
@@ -0,0 +1,19 @@
+import React, {useState} from 'react';
+
+export default function TacoCard({taco, onAddToOrder }) {
+    const { name, image, price, description} = taco
+
+
+return (
+    <div className="Card">
+        <h3>{name}</h3>
+        <img src={image} width="300" height="300"/>
+        <p>${price}</p>
+        <p>{description}</p>
+
+    <button onClick={() => onAddToOrder(taco)}>
+        Add to Order
+    </button>
+    </div>
+    )
+}
