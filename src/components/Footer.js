8321f3f1fa95e6514143a2cff0a174b3f3566b14
Maikel <maikel863@outlook.com>
2021-10-14 03:52:31 -0400

    created homepage and footer

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <div className="footer">
+            <h3>Contact the Team!</h3>
+            <p>5533 S Orange Blossom Trl suite B, Orlando, Florida 32839</p>
+            <p>123-456-7890</p>
+            <p>Mejor@tacotruck.com</p>
+        </div>
+    )
+}
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Footer from './Footer'
+export default function Header() {
+    return (
+        <div>
+            <p>El Mejor Taco Truck</p>
+            <img src="https://unsplash.com/photos/b2gdRynjL9Q" alt="Taco-Truck-Banner" />
+            <Footer />
+        </div>
+    )
+}
